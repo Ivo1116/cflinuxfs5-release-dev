b5dbe22f1b492829e898fafeaacc69351cfbd497
Radkov <ivo.radkov@sap.com>
2025-08-08 12:53:50 +0300

    rewrite for yq3

--- .github/actions/deploy-cf/register-blobs.sh
+++ .github/actions/deploy-cf/register-blobs.sh
@@ -7,14 +7,20 @@ cd "$STACK-release"
 
 GO_BLOB_FILENAME=$(cat .go_blob_name)
 
-# 1. Remove old Golang blob entries from blobs.yml (yq v4 syntax)
+# 1. Remove old Golang blob entries from blobs.yml (yq v3 syntax)
 if [[ -f config/blobs.yml ]]; then
-  yq eval 'del( .[] | select( key | startswith("golang-1-linux/") ) )' -i config/blobs.yml
+  # List all keys, filter those starting with golang-1-linux/, and delete them
+  for key in $(yq r config/blobs.yml --printMode p | grep '^golang-1-linux/'); do
+    yq d -i config/blobs.yml "$key"
+  done
 fi
 
 # 2. Remove old final build records for golang-1-linux
 rm -rf .final_builds/packages/golang-1-linux
 
 # 3. Register new blobs
-bosh add-blob "blobs/golang-1-linux/$GO_BLOB_FILENAME" "golang-1-linux/$GO_BLOB_FILENAME"
-bosh add-blob "blobs/rootfs/${STACK}-${VERSION}.tar.gz"
+bosh add-blob "blobs/golang-1-linux/$GO_BLOB_FILENAME" \
+  "golang-1-linux/$GO_BLOB_FILENAME"
+
+bosh add-blob "blobs/rootfs/${STACK}-${VERSION}.tar.gz" \
+  "rootfs/${STACK}-${VERSION}.tar.gz"
