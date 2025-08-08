0c576f03b010f4e559d2e2ce9fabc7e55ceb0b0f
Radkov <ivo.radkov@sap.com>
2025-08-08 14:42:33 +0300

    check blobs

--- .github/actions/deploy-cf/register-blobs.sh
+++ .github/actions/deploy-cf/register-blobs.sh
@@ -7,7 +7,7 @@ cd "$STACK-release"
 
 GO_BLOB_FILENAME=$(cat .go_blob_name)
 
-# 1. Remove old Golang blob entries (yq v4 syntax using regex)
+# 1. Remove old Golang blob entries
 if [[ -f config/blobs.yml ]]; then
   yq eval 'with_entries(select(.key | test("^golang-1-linux/") | not))' \
     -i config/blobs.yml
@@ -23,5 +23,11 @@ bosh add-blob "blobs/golang-1-linux/$GO_BLOB_FILENAME" \
 bosh add-blob "blobs/rootfs/${STACK}-${VERSION}.tar.gz" \
   "rootfs/${STACK}-${VERSION}.tar.gz"
 
-# 4. Upload blobs to blobstore
+# 4. Upload blobs
 bosh upload-blobs
+
+# 5. Verify Golang blob is present
+if ! bosh blobs | grep -q "golang-1-linux/$GO_BLOB_FILENAME"; then
+  echo "ERROR: Golang blob not registered correctly!"
+  exit 1
+fi
