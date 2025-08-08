a28d07a174cf4193dd5d58e0c29a4055e7df3271
Radkov <ivo.radkov@sap.com>
2025-08-08 12:50:31 +0300

    use correct yq

--- .github/actions/deploy-cf/register-blobs.sh
+++ .github/actions/deploy-cf/register-blobs.sh
@@ -7,9 +7,9 @@ cd "$STACK-release"
 
 GO_BLOB_FILENAME=$(cat .go_blob_name)
 
-# 1. Remove old Golang blob entries from blobs.yml
+# 1. Remove old Golang blob entries from blobs.yml (yq v4 syntax)
 if [[ -f config/blobs.yml ]]; then
-  yq -i 'with_entries(select(.key | startswith("golang-1-linux/") | not))' config/blobs.yml
+  yq eval 'del( .[] | select( key | startswith("golang-1-linux/") ) )' -i config/blobs.yml
 fi
 
 # 2. Remove old final build records for golang-1-linux
@@ -17,12 +17,4 @@ rm -rf .final_builds/packages/golang-1-linux
 
 # 3. Register new blobs
 bosh add-blob "blobs/golang-1-linux/$GO_BLOB_FILENAME" "golang-1-linux/$GO_BLOB_FILENAME"
-bosh add-blob "blobs/rootfs/${STACK}-${VERSION}.tar.gz" "rootfs/${STACK}-${VERSION}.tar.gz"
-
-# 4. Update spec to match the Go blob
-yq eval -i ".files = [\"golang-1-linux/$GO_BLOB_FILENAME\"]" packages/golang-1-linux/spec
-
-# 5. Sync blobs locally
-bosh sync-blobs
-
-echo "Blobs registered and synced."
+bosh add-blob "blobs/rootfs/${STACK}-${VERSION}.tar.gz"
