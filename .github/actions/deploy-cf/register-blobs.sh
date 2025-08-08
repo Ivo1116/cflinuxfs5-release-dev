5c307556d786f97afc2ffe8df02134f4b523c980
Radkov <ivo.radkov@sap.com>
2025-08-08 12:48:42 +0300

    updsate blobs

--- .github/actions/deploy-cf/register-blobs.sh
+++ .github/actions/deploy-cf/register-blobs.sh
@@ -7,17 +7,22 @@ cd "$STACK-release"
 
 GO_BLOB_FILENAME=$(cat .go_blob_name)
 
-# Remove old Golang blob entry
-yq -i 'del(.["golang-1-linux/"])' config/blobs.yml || true
+# 1. Remove old Golang blob entries from blobs.yml
+if [[ -f config/blobs.yml ]]; then
+  yq -i 'with_entries(select(.key | startswith("golang-1-linux/") | not))' config/blobs.yml
+fi
 
-# Register blobs
+# 2. Remove old final build records for golang-1-linux
+rm -rf .final_builds/packages/golang-1-linux
+
+# 3. Register new blobs
 bosh add-blob "blobs/golang-1-linux/$GO_BLOB_FILENAME" "golang-1-linux/$GO_BLOB_FILENAME"
 bosh add-blob "blobs/rootfs/${STACK}-${VERSION}.tar.gz" "rootfs/${STACK}-${VERSION}.tar.gz"
 
-# Update spec to match the Go blob
+# 4. Update spec to match the Go blob
 yq eval -i ".files = [\"golang-1-linux/$GO_BLOB_FILENAME\"]" packages/golang-1-linux/spec
 
-# Sync blobs locally
+# 5. Sync blobs locally
 bosh sync-blobs
 
 echo "Blobs registered and synced."
