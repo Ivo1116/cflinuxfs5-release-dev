4ce2edcfe2a98ca1272a6ea555ba4524015413d0
Radkov <ivo.radkov@sap.com>
2025-08-08 12:59:39 +0300

    update script

--- .github/actions/deploy-cf/register-blobs.sh
+++ .github/actions/deploy-cf/register-blobs.sh
@@ -7,15 +7,14 @@ cd "$STACK-release"
 
 GO_BLOB_FILENAME=$(cat .go_blob_name)
 
-# 1. Remove old Golang blob entries from blobs.yml (yq v3 syntax)
+# 1. Remove old Golang blob entries (yq v3 syntax)
 if [[ -f config/blobs.yml ]]; then
-  # List all keys, filter those starting with golang-1-linux/, and delete them
   for key in $(yq r config/blobs.yml --printMode p | grep '^golang-1-linux/'); do
     yq d -i config/blobs.yml "$key"
   done
 fi
 
-# 2. Remove old final build records for golang-1-linux
+# 2. Remove old final build records
 rm -rf .final_builds/packages/golang-1-linux
 
 # 3. Register new blobs
@@ -24,3 +23,6 @@ bosh add-blob "blobs/golang-1-linux/$GO_BLOB_FILENAME" \
 
 bosh add-blob "blobs/rootfs/${STACK}-${VERSION}.tar.gz" \
   "rootfs/${STACK}-${VERSION}.tar.gz"
+
+# 4. Upload blobs to blobstore
+bosh upload-blobs
